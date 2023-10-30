a16f02fb72feae4e4f5b2fe8fadc3bc3d590155e
pabm2pro <pablo.2221@hotmail.com>
2023-10-30 16:51:48 -0600

    initial commit

--- src/pruebasUnitarias/HuespedTest.java
+++ src/pruebasUnitarias/HuespedTest.java
@@ -10,19 +10,19 @@ public class HuespedTest {
     @Test
     public void testConstructor() {
         // Creamos una instancia de Huesped con valores iniciales
-        Huesped huesped = new Huesped("Nombre Apellido", "1 2345 6789", "45676789");
+        Huesped huesped = new Huesped("Pablo Quiros", "123456789", "45676789");
 
         // Verificamos que los valores iniciales sean los correctos
-        assertEquals("Nombre Apellido", huesped.getNombre());
-        assertEquals("1 2345 6789", huesped.getDocumento());
+        assertEquals("Pablo Quiros", huesped.getNombre());
+        assertEquals("123456789", huesped.getDocumento());
         assertEquals("45676789", huesped.getTelefono());
     }
 
-    //verificar que el método 'setTelefono' modifica el teléfono del huésped
+    //verificar que el método setTelefono modifica el teléfono del huésped
     @Test
     public void testSetTelefono() {
         // Creamos una instancia de Huesped y modificamos su número de teléfono
-        Huesped huesped = new Huesped("Nombre Apellido", "1 2345 6789", "45676789");
+        Huesped huesped = new Huesped("Andres Carvajal", "123456789", "45676789");
         huesped.setTelefono("98765432");
 
         // Verificamos que el número de teléfono haya sido modificado correctamente
